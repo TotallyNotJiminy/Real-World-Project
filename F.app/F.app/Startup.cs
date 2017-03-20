facceb23f6e1825a28db6e387f626cbade77e6d4
Aivaras Jonikas <Aivaras Jonikas>
2017-03-20 16:25:23 +0000

    Added fapp

---
+++ F.app/F.app/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(F.app.Startup))]
+namespace F.app
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
